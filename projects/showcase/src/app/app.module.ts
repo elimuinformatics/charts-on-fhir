a207e99731bcda67ca84583ec170f61e15effd4c
Colin Stynes <cstynes@elimu.io>
2022-11-09 11:06:31 -0500

    import library in showcase app

--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsOnFhirModule } from 'ngx-charts-on-fhir';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgxChartsOnFhirModule],
   providers: [],
   bootstrap: [AppComponent],
 })
