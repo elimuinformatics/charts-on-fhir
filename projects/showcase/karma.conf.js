f844058dc5fc47bce55c9cb14b21e2296c893773
Colin Stynes <cstynes@elimu.io>
2022-12-07 16:06:15 -0500

    lcov report + added sonar to github workflow

--- projects/showcase/karma.conf.js
+++ projects/showcase/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "../../coverage/showcase"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "lcov" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
