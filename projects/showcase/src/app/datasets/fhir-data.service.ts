ed02b3ba13bce4f535d683721dc12daac42e8729
yashpmohod <ymohod@elimu.io>
2022-12-14 14:31:09 +0530

    RT-506:Medications chart

--- projects/showcase/src/app/datasets/fhir-data.service.ts
+++ projects/showcase/src/app/datasets/fhir-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Bundle, Observation } from 'fhir/r2';
+import { Bundle, MedicationOrder, Observation } from 'fhir/r2';
 import { delay, EMPTY, expand, Observable, of, tap } from 'rxjs';
 
 const server = 'https://fhir2-internal.elimuinformatics.com/baseDstu2';
@@ -16,6 +16,13 @@ export class FhirDataService {
 
   getObservations() {
     return this.http.get<Bundle<Observation>>(`${server}/Observation?patient=${patient}&_count=100`).pipe(
+      expand((result) => this.getNextBundle(result).pipe(delay(100))) // todo: remove this delay
+    );
+  }
+
+
+  getMedicationsOrder() {
+    return this.http.get<Bundle<MedicationOrder>>(`${server}/MedicationOrder?patient=${patient}&_count=100`).pipe(
       expand((result) => this.getNextBundle(result).pipe(delay(100))) // todo: remove this delay
     );
   }
