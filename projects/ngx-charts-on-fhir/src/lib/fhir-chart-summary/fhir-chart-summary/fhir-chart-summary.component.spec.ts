502fa212e06df8a5520da8f764ce29193c9804fb
Colin Stynes <cstynes@elimu.io>
2022-12-15 10:34:24 -0500

    fixed failing tests

--- projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.spec.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { ManagedDataLayer } from '../../data-layer/data-layer';
+import { DataLayerManagerService } from '../../data-layer/data-layer-manager.service';
 import { FhirChartSummaryComponent } from './fhir-chart-summary.component';
+
+class MockLayerManager {
+  selectedLayers$ = new BehaviorSubject<ManagedDataLayer[]>([]);
+  timelineRange$ = new ReplaySubject<{ min: number; max: number }>();
+}
 
 describe('FhirChartSummaryComponent', () => {
   let component: FhirChartSummaryComponent;
   let fixture: ComponentFixture<FhirChartSummaryComponent>;
+  let layerManager: MockLayerManager;
 
   beforeEach(async () => {
+    layerManager = new MockLayerManager();
     await TestBed.configureTestingModule({
-      declarations: [ FhirChartSummaryComponent ]
-    })
-    .compileComponents();
+      declarations: [FhirChartSummaryComponent],
+      providers: [{ provide: DataLayerManagerService, useValue: layerManager }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FhirChartSummaryComponent);
     component = fixture.componentInstance;
