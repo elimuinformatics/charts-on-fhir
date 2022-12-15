f2c4cc26b0dbd1418132abfb70f05de27c1edecc
Colin Stynes <cstynes@elimu.io>
2022-12-15 13:37:53 -0500

    created VisibleDataService, added unit tests

--- projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.spec.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.spec.ts
@@ -1,24 +1,37 @@
+import { Component, DebugElement, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
-import { ManagedDataLayer } from '../../data-layer/data-layer';
-import { DataLayerManagerService } from '../../data-layer/data-layer-manager.service';
+import { By } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import { VisibleData, VisibleDataService } from '../../data-layer/visible-data.service';
 import { FhirChartSummaryComponent } from './fhir-chart-summary.component';
 
-class MockLayerManager {
-  selectedLayers$ = new BehaviorSubject<ManagedDataLayer[]>([]);
-  timelineRange$ = new ReplaySubject<{ min: number; max: number }>();
+class MockVisibleDataService {
+  visible$ = new BehaviorSubject<VisibleData[]>([]);
+}
+
+@Component({ selector: 'analysis-card', template: '<ng-content></ng-content>' })
+class MockAnalysisCardComponent {
+  @Input() dataset: unknown;
+}
+
+@Component({ selector: 'statistics', template: '' })
+class MockStatisticsComponent {
+  @Input() layer: unknown;
+  @Input() dataset: unknown;
+  @Input() visibleData: unknown;
+  @Input() dateRange: unknown;
 }
 
 describe('FhirChartSummaryComponent', () => {
   let component: FhirChartSummaryComponent;
   let fixture: ComponentFixture<FhirChartSummaryComponent>;
-  let layerManager: MockLayerManager;
+  let visibleDataService: MockVisibleDataService;
 
   beforeEach(async () => {
-    layerManager = new MockLayerManager();
+    visibleDataService = new MockVisibleDataService();
     await TestBed.configureTestingModule({
-      declarations: [FhirChartSummaryComponent],
-      providers: [{ provide: DataLayerManagerService, useValue: layerManager }],
+      declarations: [FhirChartSummaryComponent, MockAnalysisCardComponent, MockStatisticsComponent],
+      providers: [{ provide: VisibleDataService, useValue: visibleDataService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FhirChartSummaryComponent);
@@ -28,5 +41,39 @@ describe('FhirChartSummaryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a card for each dataset', () => {
+    const emptyVisibleData: any = { layer: {}, dataset: {}, data: [], dateRange: {} };
+    visibleDataService.visible$.next([emptyVisibleData, emptyVisibleData, emptyVisibleData]);
+    fixture.detectChanges();
+    const cards = fixture.debugElement.queryAll(By.directive(MockAnalysisCardComponent));
+    expect(cards.length).toBe(3);
+  });
+
+  it('should set inputs on analysis-card component', () => {
+    const visibleData: any = {
+      dataset: 'dataset',
+    }
+    visibleDataService.visible$.next([visibleData]);
+    fixture.detectChanges();
+    const card: DebugElement = fixture.debugElement.query(By.directive(MockAnalysisCardComponent));
+    expect(card.componentInstance.dataset).toEqual('dataset');
+  });
+
+  it('should set inputs on statistics component', () => {
+    const visibleData: any = {
+      layer: 'layer',
+      dataset: 'dataset',
+      data: 'data',
+      dateRange: 'dateRange',
+    };
+    visibleDataService.visible$.next([visibleData]);
+    fixture.detectChanges();
+    const statistics: DebugElement = fixture.debugElement.query(By.directive(MockStatisticsComponent));
+    expect(statistics.componentInstance.layer).toEqual('layer');
+    expect(statistics.componentInstance.dataset).toEqual('dataset');
+    expect(statistics.componentInstance.visibleData).toEqual('data');
+    expect(statistics.componentInstance.dateRange).toEqual('dateRange');
   });
 });
--- projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-summary/fhir-chart-summary/fhir-chart-summary.component.ts
@@ -1,17 +1,5 @@
 import { Component } from '@angular/core';
-import { Chart, ScatterDataPoint } from 'chart.js';
-import { map, OperatorFunction, pipe, combineLatestWith, Observable, tap } from 'rxjs';
-import { DateRange } from '../../analysis/analysis-card-content.component';
-import { DataLayer, Dataset, ManagedDataLayer } from '../../data-layer/data-layer';
-import { DataLayerManagerService } from '../../data-layer/data-layer-manager.service';
-import { MILLISECONDS_PER_DAY } from '../../utils';
-
-type CardContext = {
-  layer: DataLayer;
-  dataset: Dataset;
-  visibleData: ScatterDataPoint[];
-  dateRange: DateRange;
-};
+import { VisibleDataService } from '../../data-layer/visible-data.service';
 
 @Component({
   selector: 'fhir-chart-summary',
@@ -19,49 +7,6 @@ type CardContext = {
   styleUrls: ['./fhir-chart-summary.component.css'],
 })
 export class FhirChartSummaryComponent {
-  constructor(private layerManager: DataLayerManagerService) {}
-  cardContexts$ = this.layerManager.selectedLayers$.pipe(visibleDatasets(), filterByDateRange(this.layerManager.timelineRange$));
+  constructor(public visibleDataService: VisibleDataService) {}
 }
 
-function visibleDatasets(): OperatorFunction<ManagedDataLayer[], { layer: DataLayer; dataset: Dataset }[]> {
-  return pipe(
-    map((layers) =>
-      layers
-        .filter((layer) => layer.enabled)
-        .flatMap((layer) =>
-          layer.datasets
-            .filter((dataset) => !dataset.hidden)
-            .sort(byMostRecentValue)
-            .map((dataset) => ({ layer, dataset }))
-        )
-    )
-  );
-}
-
-/** Comparison function that can be used to sort Datasets by their most recent y-value, descending */
-function byMostRecentValue(a: Dataset, b: Dataset) {
-  const ay = a.data.at(-1)?.y;
-  const by = b.data.at(-1)?.y;
-  if (ay == null || by == null) {
-    return 0;
-  }
-  return by - ay;
-}
-
-function filterByDateRange(dateRange$: Observable<{ min: number; max: number }>): OperatorFunction<{ layer: DataLayer; dataset: Dataset }[], CardContext[]> {
-  return pipe(
-    combineLatestWith(dateRange$),
-    map(([datasets, range]) =>
-      datasets.map(({ layer, dataset }) => ({
-        layer,
-        dataset,
-        visibleData: dataset.data.filter((point) => range.min <= point.x && point.x <= range.max),
-        dateRange: {
-          min: new Date(range.min),
-          max: new Date(range.max),
-          days: Math.ceil((range.max - range.min) / MILLISECONDS_PER_DAY),
-        },
-      }))
-    )
-  );
-}
