8c9f67270a064ba6029f0ea3219b924e528c2818
yashpmohod <yashm@elimu.io>
2022-12-22 13:25:19 +0530

    RT-507:Loading indicators, resolve colin remaining comments

--- projects/ngx-charts-on-fhir/src/lib/data-layer-browser/data-layer-browser.module.ts
+++ projects/ngx-charts-on-fhir/src/lib/data-layer-browser/data-layer-browser.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataLayerBrowserComponent } from './data-layer-browser.component';
 import { MatIconModule } from '@angular/material/icon';
