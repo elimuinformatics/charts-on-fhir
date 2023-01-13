65226f9ec482ff61647740b13fab48160c9283fc
Colin Stynes <cstynes@elimu.io>
2023-01-12 21:48:59 -0500

    unit tests

--- projects/ngx-charts-on-fhir/src/lib/fhir-chart/medication-scale.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart/medication-scale.ts
@@ -12,7 +12,7 @@ declare module 'chart.js' {
 /** Medication scale adds extra padding to account for the wide lines and label offset */
 export class MedicationScale extends CategoryScale {
   static override id = 'medication';
-  override getPixelForValue(value: number, index?: number | undefined): number {
+  override getPixelForValue(value: any, index?: number | undefined): number {
     const extraPadding = 30;
     const pixel = super.getPixelForValue(value, index);
     const shrink = (this.height - extraPadding) / this.height;
