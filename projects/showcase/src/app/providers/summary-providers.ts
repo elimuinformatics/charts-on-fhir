db8c02f0a9ac9e24be9fc5f658815b5c6f1bb278
Colin Stynes <cstynes@elimu.io>
2023-01-24 15:16:03 -0500

    medication summary (#63)

--- projects/showcase/src/app/providers/summary-providers.ts
+++ projects/showcase/src/app/providers/summary-providers.ts
@@ -1,5 +1,6 @@
-import { ScatterDataPointSummaryService, SummaryService } from 'ngx-charts-on-fhir';
+import { MedicationSummaryService, ScatterDataPointSummaryService, SummaryService } from 'ngx-charts-on-fhir';
 
 export const summaryProviders = [
+  { provide: SummaryService, useExisting: MedicationSummaryService, multi: true },
   { provide: SummaryService, useExisting: ScatterDataPointSummaryService, multi: true },
 ];
