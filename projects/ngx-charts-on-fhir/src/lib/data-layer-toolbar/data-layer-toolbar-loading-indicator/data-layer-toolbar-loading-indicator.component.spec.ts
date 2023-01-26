545dbbc8cae75c322f2b0dbe522fbad28a6274bc
Colin Stynes <cstynes@elimu.io>
2023-01-26 16:41:35 -0500

    fix tests, code cleanup

--- projects/ngx-charts-on-fhir/src/lib/data-layer-toolbar/data-layer-toolbar-loading-indicator/data-layer-toolbar-loading-indicator.component.spec.ts
+++ projects/ngx-charts-on-fhir/src/lib/data-layer-toolbar/data-layer-toolbar-loading-indicator/data-layer-toolbar-loading-indicator.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { DataLayerToolbarLoadingIndicatorComponent } from './data-layer-toolbar-loading-indicator.component';
 
@@ -8,9 +11,9 @@ describe('DataLayerToolbarLoadingIndicatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DataLayerToolbarLoadingIndicatorComponent ]
-    })
-    .compileComponents();
+      declarations: [DataLayerToolbarLoadingIndicatorComponent],
+      imports: [MatIconModule, MatProgressSpinnerModule, MatTooltipModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DataLayerToolbarLoadingIndicatorComponent);
     component = fixture.componentInstance;
--- projects/ngx-charts-on-fhir/src/lib/data-layer-toolbar/data-layer-toolbar-loading-indicator/data-layer-toolbar-loading-indicator.component.ts
+++ projects/ngx-charts-on-fhir/src/lib/data-layer-toolbar/data-layer-toolbar-loading-indicator/data-layer-toolbar-loading-indicator.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'data-layer-toolbar-loading-indicator',
   templateUrl: './data-layer-toolbar-loading-indicator.component.html',
-  styleUrls: ['./data-layer-toolbar-loading-indicator.component.css']
+  styleUrls: ['./data-layer-toolbar-loading-indicator.component.css'],
 })
 export class DataLayerToolbarLoadingIndicatorComponent {
   @Input() loading: boolean | null = null;
