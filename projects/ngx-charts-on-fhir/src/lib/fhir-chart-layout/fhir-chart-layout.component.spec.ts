79e1b54aaf73944d28b73c420df5b02b960452e5
Colin Stynes <cstynes@elimu.io>
2023-02-01 17:57:43 -0500

    fixed layout unit test

--- projects/ngx-charts-on-fhir/src/lib/fhir-chart-layout/fhir-chart-layout.component.spec.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-layout/fhir-chart-layout.component.spec.ts
@@ -1,6 +1,21 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FhirChartLayoutComponent } from './fhir-chart-layout.component';
+
+@Component({ selector: 'data-layer-toolbar', template: '' })
+class MockDataLayerToolbarComponent {
+  @Input() active?: any;
+  @Input() buttons?: any;
+}
+
+@Component({ selector: 'data-layer-browser', template: '' })
+class MockDataLayerBrowserComponent {}
+
+@Component({ selector: 'data-layer-list', template: '' })
+class MockDataLayerListComponent {}
 
 describe('FhirChartLayoutComponent', () => {
   let component: FhirChartLayoutComponent;
@@ -8,9 +23,9 @@ describe('FhirChartLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FhirChartLayoutComponent ]
-    })
-    .compileComponents();
+      declarations: [FhirChartLayoutComponent, MockDataLayerToolbarComponent, MockDataLayerBrowserComponent, MockDataLayerListComponent],
+      imports: [NoopAnimationsModule, MatSidenavModule, MatIconModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FhirChartLayoutComponent);
     component = fixture.componentInstance;
