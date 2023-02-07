50c7d86dddb1dd77186952ef7140f687b9e7c9cb
yashpmohod <117890382+yashpmohod@users.noreply.github.com>
2023-02-07 19:45:06 +0530

    RT-622:BP entry form (#86)
    * RT-622-BP-entry-form,BP-entry-form-created
    
    * RT-622:BP entry form,Fixed width issue and change the error message
    
    * RT-622-BP-entry-form,Added validation for form
    
    * RT-622-BP-entry-form,test cases issue fixed and remove form duplication from css file
    
    * RT-622-BP-entry-form,Updated changes
    
    * RT-622-BP-entry-form,updated changes as per suggestions
    
    * RT-622-BP-entry-form
    
    * RT-622-BP-entry-form
    
    ---------
    
    Co-authored-by: yashpmohod <yashm@elimu.io>

---
+++ projects/cardiovascular-patient-app/src/app/report-bp/report-bp.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { DataLayerManagerService } from 'ngx-charts-on-fhir';
+
+const BloodPressureRangeValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const systolic = control.get('systolic');
+  const diastolic = control.get('diastolic');
+
+  return systolic && diastolic && systolic.value > diastolic.value ? null : { bloodPressure: true };
+};
+
+@Component({
+  selector: 'report-bp',
+  templateUrl: './report-bp.component.html',
+  styleUrls: ['./report-bp.component.css'],
+})
+export class ReportBPComponent implements OnInit {
+  submitted = false;
+  min = 11;
+  max = 250;
+
+  form = this.fb.group(
+    {
+      systolic: [null, [Validators.required, Validators.min(this.min), Validators.max(this.max)]],
+      diastolic: [null, [Validators.required, Validators.min(this.min), Validators.max(this.max)]],
+    },
+    { validators: [BloodPressureRangeValidator] }
+  );
+
+  constructor(private fb: FormBuilder, private layerManager: DataLayerManagerService) {}
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((value) => {
+      console.log(this.layerManager.availableLayers$);
+      this.updateBPentryForm(value);
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    this.form.reset();
+  }
+
+  private updateBPentryForm(formValue: typeof this.form.value): void {
+    console.log(formValue);
+  }
+}
