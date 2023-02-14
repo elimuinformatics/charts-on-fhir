87b5f26dee7b9d51a816fb99b271b428d567505b
yashpmohod <yashm@elimu.io>
2023-02-14 17:25:21 +0530

    RT-621-Last-reported-New:update Test cases

--- projects/cardiovascular-patient-app/src/app/lastreport-bp/lastreport-bp.component.spec.ts
+++ projects/cardiovascular-patient-app/src/app/lastreport-bp/lastreport-bp.component.spec.ts
@@ -1,15 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LastReportBPComponent } from './lastreport-bp.component';
 import { MatCardModule } from '@angular/material/card';
+import { DataLayerManagerService } from 'ngx-charts-on-fhir';
+import { EMPTY, map } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+const mockLayerManager = {
+  availableLayers$: EMPTY,
+  selectedLayers$: EMPTY,
+  allLayers$: EMPTY
+};
 
 describe('LastReportBPComponent', () => {
   let component: LastReportBPComponent;
   let fixture: ComponentFixture<LastReportBPComponent>;
+  let scheduler: TestScheduler;
 
   beforeEach(async () => {
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
     await TestBed.configureTestingModule({
       imports: [MatCardModule],
       declarations: [LastReportBPComponent],
+      providers: [{ provide: DataLayerManagerService, useValue: mockLayerManager },],
+
     }).compileComponents();
 
     fixture = TestBed.createComponent(LastReportBPComponent);
@@ -20,4 +35,65 @@ describe('LastReportBPComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should extract the last data point of each dataset in the "Blood Pressure" layer', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+      const layers$ = cold('a', {
+        a: [
+          {
+            name: 'a',
+            id: '123',
+            enabled: true,
+            datasets: [
+              {
+                data: [
+                  { x: 10, y: 100 },
+                  { x: 20, y: 200 },
+                ],
+              },
+              {
+                data: [
+                  { x: 10, y: 100 },
+                  { x: 20, y: 200 },
+                ],
+              },
+            ],
+            scale: { id: 'a' },
+          },
+          {
+            name: 'Blood Pressure',
+            id: '567',
+            enabled: true,
+            datasets: [
+              {
+                data: [
+                  { x: 1352359652, y: 120 },
+                  { x: 1352359652, y: 122 },
+                ],
+              },
+              {
+                data: [
+                  { x: 1476359652, y: 80 },
+                  { x: 1476359652, y: 78 },
+                ],
+              },
+            ],
+            scale: { id: 'a' },
+          }
+        ],
+      });
+
+      const result$ = layers$.pipe(
+        map((layers) =>
+          layers
+            .filter((layer) => layer.name === 'Blood Pressure')
+            .map((layer) => layer.datasets.map((data) => data.data))
+            .map((layer) => layer.map((data) => data.slice(-1)))
+        )
+      );
+      expectObservable(result$).toBe('a', {
+        a: [[[{ x: 1352359652, y: 122 }], [{ x: 1476359652, y: 78 }]]],
+      });
+    });
+  })
 });
