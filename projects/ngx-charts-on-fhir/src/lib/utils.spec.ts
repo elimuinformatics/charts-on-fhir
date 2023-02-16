84276d5391e2c8c389ed617904389a265a920f68
yashpmohod <yashm@elimu.io>
2023-02-16 19:37:17 +0530

    RT-621-Last-reported-New

--- projects/ngx-charts-on-fhir/src/lib/utils.spec.ts
+++ projects/ngx-charts-on-fhir/src/lib/utils.spec.ts
@@ -1,14 +1,14 @@
-import { formatDate,formatTime } from './utils';
+import { formatDate, formatTime } from './utils';
 
 describe('utils', () => {
 
     it('should convert timestamp in expected timeformat', () => {
-        const expectedTime =formatTime(1676547182000);
+        const expectedTime = formatTime(1676547182000);
         expect(expectedTime).toEqual('11:33 AM')
-     });
+    });
 
     it('should convert timestamp in expected dateformat', () => {
-       const expectedDate =formatDate(1676547182000);
-       expect(expectedDate).toEqual('16 Feb 2023')
+        const expectedDate = formatDate(1676547182000);
+        expect(expectedDate).toEqual('16 Feb 2023')
     });
 });
