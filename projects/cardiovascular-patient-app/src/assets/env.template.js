6af7db1fc6e9bb7cf8338e4d07ac6380453c5247
yashpmohod <yashm@elimu.io>
2023-02-22 19:54:34 +0530

    RT-620-Banner-title

--- projects/cardiovascular-patient-app/src/assets/env.template.js
+++ projects/cardiovascular-patient-app/src/assets/env.template.js
@@ -1,10 +1,4 @@
-// window.env = {
-//     clientId: "${CLIENT_ID}",
-//     appTitle: "${APP_TITLE}"
-//   };
-
-(function (window) {
-  window["env"] = window["env"] || {};
-  window["env"]['clientId'] = "${CLIENT_ID}";
-  window["env"]['appTitle'] = "${APP_TITLE}";
-})(this);
+window.env = {
+    clientId: "${CLIENT_ID}",
+    appTitle: "${APP_TITLE}"
+  };
