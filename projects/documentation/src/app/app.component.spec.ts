6c25a3728823f0d6387eeebf2d8aac7f6caa7ba9
Colin Stynes <cstynes@elimu.io>
2023-03-08 19:15:32 -0500

    fixed unit tests

--- projects/documentation/src/app/app.component.spec.ts
+++ projects/documentation/src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NgDocModule } from '@ng-doc/app';
+import { NgDocSidebarModule } from '@ng-doc/app/components/sidebar';
+import { NgDocNavbarModule } from '@ng-doc/app/components/navbar';
+import { NgDocGeneratedModule } from '@ng-doc/generated';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, NoopAnimationsModule, NgDocNavbarModule, NgDocSidebarModule, NgDocModule.forRoot(), NgDocGeneratedModule.forRoot()],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -14,18 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'documentation'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('documentation');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('documentation app is running!');
   });
 });
