1846966efd3a87797ceef6bb88e4fb35164b7cdc
Colin Stynes <cstynes@elimu.io>
2023-03-08 22:33:42 -0500

    shared karma.conf (3rd try)

--- projects/showcase/karma.conf.js
+++ projects/showcase/karma.conf.js
@@ -1,4 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = (config) => require("../../karma.conf.js")(config, "showcase");
+module.exports = function (config) {
+  config.set({
+    ...require("../../karma.base.conf.js")(config, "showcase"),
+  });
+};
