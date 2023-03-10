a59468ee5b14e5f10a30f6a48b8b54844a0aeab3
yashpmohod <yashm@elimu.io>
2023-03-10 18:07:37 +0530

    RT-672-last-reported-BP-changes

--- projects/cardiovascular-patient-app/src/app/datasets/observations.service.ts
+++ projects/cardiovascular-patient-app/src/app/datasets/observations.service.ts
@@ -34,6 +34,6 @@ export class ObservationLayerService extends DataLayerService {
   getQueryfromCoding(codings: Coding[]) {
     let finalUrl = '?code=';
     codings.forEach((coding: any) => (finalUrl += `${coding.system}|${coding.code},`));
-    return finalUrl;
+    return finalUrl+'&_sort=-date';
   }
 }
