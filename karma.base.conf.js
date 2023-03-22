fa81e8bf6be69cb16f582b964e0478f4cd772fb1
yashpmohod <yashm@elimu.io>
2023-03-22 15:04:00 +0530

    RT-689-fail-specs-with-no-expectations

--- karma.base.conf.js
+++ karma.base.conf.js
@@ -17,6 +17,7 @@ module.exports = (config, project) => ({
       // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
       // for example, you can disable the random execution with `random: false`
       // or set a specific seed with `seed: 4321`
+      failSpecWithNoExpectations: true
     },
     clearContext: false, // leave Jasmine Spec Runner output visible in browser
   },
