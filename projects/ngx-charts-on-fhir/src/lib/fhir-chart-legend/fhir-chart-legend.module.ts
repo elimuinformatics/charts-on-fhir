8235c88b7cbf8e8d13ec3120d627f0cde73dc6b7
yashpmohod <yashm@elimu.io>
2023-03-23 16:41:10 +0530

    RT-691-Legend-sort-order

--- projects/ngx-charts-on-fhir/src/lib/fhir-chart-legend/fhir-chart-legend.module.ts
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-legend/fhir-chart-legend.module.ts
@@ -4,9 +4,10 @@ import { FhirChartLegendComponent } from './fhir-chart-legend.component';
 import { MatIconModule } from '@angular/material/icon';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { MatButtonModule } from '@angular/material/button';
+import { SortByPipe } from './sort-by.pipe';
 
 @NgModule({
-  declarations: [FhirChartLegendComponent],
+  declarations: [FhirChartLegendComponent,SortByPipe],
   imports: [CommonModule, MatIconModule, MatButtonModule, OverlayModule],
   exports: [FhirChartLegendComponent],
 })
---
+++ projects/ngx-charts-on-fhir/src/lib/fhir-chart-legend/sort-by.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { orderBy, sortBy } from 'lodash';
+
+@Pipe({ name: 'sortBy' })
+export class SortByPipe implements PipeTransform {
+
+    transform(value: any[], order: any = '', column: string = ''): any[] {
+        if (!value || order === '' || !order) { return value; } // no array
+        if (!column || column === '') {
+            if (order === 'asc') { return value.sort() }
+            else { return value.sort().reverse(); }
+        } // sort 1d array
+        if (value.length <= 1) { return value; } // array with only one item
+        return orderBy(value, [column], [order]);
+    }
+}
