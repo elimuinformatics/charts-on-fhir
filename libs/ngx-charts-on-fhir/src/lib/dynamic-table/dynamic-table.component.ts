3c3086fcc5c8dbeda1d7b9503090d665ecce9a32
Colin Stynes <cstynes@elimu.io>
2023-04-20 21:13:52 -0400

    dynamic table grid layout and 2d array support

--- libs/ngx-charts-on-fhir/src/lib/dynamic-table/dynamic-table.component.ts
+++ libs/ngx-charts-on-fhir/src/lib/dynamic-table/dynamic-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { zipObject } from 'lodash-es';
 
 @Component({
   selector: 'dynamic-table',
@@ -6,8 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./dynamic-table.component.css'],
 })
 export class DynamicTableComponent {
-  @Input() data: Record<string, string>[] = [{}];
-  get columns() {
-    return Object.keys(this.data[0]);
+  columns: string[] = [];
+  _data: Record<string, string>[] = [];
+  @Input() set data(inputData: Record<string, string>[] | string[][]) {
+    if (isArrayData(inputData)) {
+      this._data = inputData.slice(1).map((row) => zipObject(inputData[0], row));
+      this.columns = inputData[0];
+    } else {
+      this._data = inputData;
+      this.columns = Object.keys(this._data[0]);
+    }
   }
+}
+
+function isArrayData(data: Record<string, string>[] | string[][]): data is string[][] {
+  return Array.isArray(data[0]);
 }
