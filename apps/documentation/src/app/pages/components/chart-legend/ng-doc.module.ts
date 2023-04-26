ea2971a3feac06c0b3be871dc59483ea65afd600
Colin Stynes <cstynes@elimu.io>
2023-04-26 13:01:25 -0400

    provide separate services for each doc module

--- apps/documentation/src/app/pages/components/chart-legend/ng-doc.module.ts
+++ apps/documentation/src/app/pages/components/chart-legend/ng-doc.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartLegendDemoComponent } from './demos/legend/chart-legend-demo.component';
 import { ChartTagsLegendDemoComponent } from './demos/tags-legend/chart-tags-legend-demo.component';
-import { FhirChartLegendModule } from '@elimuinformatics/ngx-charts-on-fhir';
+import {
+  DataLayerManagerService,
+  FhirChartLegendModule,
+  FhirChartTagsService,
+} from '@elimuinformatics/ngx-charts-on-fhir';
 
 @NgModule({
   imports: [CommonModule, FhirChartLegendModule],
-  // Declare you demo components here
   declarations: [ChartLegendDemoComponent, ChartTagsLegendDemoComponent],
+  providers: [DataLayerManagerService, FhirChartTagsService],
 })
 export class ChartLegendDemoModule {}
