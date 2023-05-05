e4f4d580894b8e03901c9be7e6692fe1ee5a76da
amolht <57406228+amolht@users.noreply.github.com>
2023-05-05 18:26:38 +0530

    RT-725-Asterisks-on-medication-names (#190)
    * RT-725-Asterisks-on-medication-names
    
    * added group for groupBy medication lable
    
    * Remove code smell
    
    * update a code and addedn ' (Est. Duration)' in lable

--- libs/ngx-charts-on-fhir/src/lib/fhir-chart-summary/medication-summary.service.ts
+++ libs/ngx-charts-on-fhir/src/lib/fhir-chart-summary/medication-summary.service.ts
@@ -3,6 +3,7 @@ import { formatDate } from '../utils';
 import { MedicationDataPoint } from '../fhir-mappers/medication-request/simple-medication-mapper.service';
 import { DataLayer, TimelineChartType } from '../data-layer/data-layer';
 import { SummaryService } from './summary.service';
+import { groupBy } from 'lodash-es';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,9 @@ export class MedicationSummaryService implements SummaryService {
     return layer.scale.type === 'category';
   }
   summarize(layer: DataLayer<TimelineChartType, MedicationDataPoint[]>): Record<string, string>[] {
-    return layer.datasets.map((dataset) => ({
+    const groups = groupBy(layer.datasets, (dataset) => dataset.chartsOnFhir?.group ?? dataset.label);
+    const datasets = Object.entries(groups).map(([label, ds]) => ({ ...ds[0], label, data: ds.flatMap((d) => d.data) }));
+    return datasets.map((dataset) => ({
       [layer.name]: dataset.label ?? '(unknown)',
       'First Written': formatDate(Math.min(...dataset.data.map((point) => point.authoredOn))),
       'Last Written': formatDate(Math.max(...dataset.data.map((point) => point.authoredOn))),
