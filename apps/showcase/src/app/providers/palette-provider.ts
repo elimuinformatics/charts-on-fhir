408981b25c202c2472c192985766a6b9997cfff2
Colin Stynes <cstynes@elimu.io>
2023-05-08 16:13:45 -0400

    Improve color contrast (#200)

--- apps/showcase/src/app/providers/palette-provider.ts
+++ apps/showcase/src/app/providers/palette-provider.ts
@@ -1,6 +1,2 @@
-// https://colorbrewer2.org/
-const brewerDarkPalette = ['#e36667', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#c36d3c', '#f781bf', '#999999'];
-const brewerLightPalette = ['#8dd3c7', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9'];
-const brewerPalette = [...brewerDarkPalette, ...brewerLightPalette];
-
-export default brewerPalette;
+const palette = ['#e36667', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#c36d3c', '#f781bf', '#c46358', '#5a84a1', '#ba803f', '#90b354', '#ab7490'];
+export default palette;
