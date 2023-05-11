58e8bb4881abadeb6aa976c7adc2fce0f16c54e4
Colin Stynes <cstynes@elimu.io>
2023-05-11 10:51:28 -0400

    test npm package in docker container

--- libs/ngx-charts-on-fhir/test-npm-package.sh
+++ libs/ngx-charts-on-fhir/test-npm-package.sh
@@ -14,23 +14,13 @@ PACKAGE_FILE=$(npm pack)
 cd ../../..
 
 echo ::::: Creating a new Angular app
-mkdir -p tmp
-cd tmp
-rm -rf ./test-app
 npx --yes \@angular/cli@${MIN_ANGULAR_VERSION} new test-app --defaults
 cd test-app
 
 echo ::::: Installing Charts-on-FHIR library
-npm i ../../dist/libs/ngx-charts-on-fhir/${PACKAGE_FILE}
+npm i ../dist/libs/ngx-charts-on-fhir/${PACKAGE_FILE}
 
 echo ::::: Building the Angular app
 npx ng build
-
-echo ::::: Cleaning up temporary files
-cd ..
-rm -rf ./test-app
-cd ..
-rmdir --ignore-fail-on-non-empty tmp
-rm dist/libs/ngx-charts-on-fhir/${PACKAGE_FILE}
 
 echo ::::: Success
