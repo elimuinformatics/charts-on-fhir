7c911214a0e18dda6d4051ec0af9241a3f5ac3d8
Colin Stynes <cstynes@elimu.io>
2023-08-30 15:53:03 -0400

    responsive range selector

--- libs/ngx-charts-on-fhir/src/lib/timeline-range-selector/timeline-range-selector.module.ts
+++ libs/ngx-charts-on-fhir/src/lib/timeline-range-selector/timeline-range-selector.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelineRangeSelectorComponent } from './timeline-range-selector.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DefaultMatCalendarRangeStrategy, MAT_DATE_RANGE_SELECTION_STRATEGY, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [TimelineRangeSelectorComponent],
-  imports: [CommonModule, MatDatepickerModule, MatInputModule, MatButtonToggleModule, MatNativeDateModule, FormsModule],
+  imports: [CommonModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatButtonToggleModule, MatMenuModule],
   exports: [TimelineRangeSelectorComponent],
+  // providers: [
+  //   {
+  //     provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+  //     useClass: DefaultMatCalendarRangeStrategy,
+  //   },
+  // ],
 })
 export class TimelineRangeSelectorModule {}
