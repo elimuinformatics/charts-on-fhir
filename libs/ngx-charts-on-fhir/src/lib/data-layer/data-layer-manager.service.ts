60b502178158fe781215d1a93eef38ad21b27823
yashpmohod <ymohod@elimu.io>
2023-09-01 13:34:45 +0530

    Updated an version

--- libs/ngx-charts-on-fhir/src/lib/data-layer/data-layer-manager.service.ts
+++ libs/ngx-charts-on-fhir/src/lib/data-layer/data-layer-manager.service.ts
@@ -143,7 +143,10 @@ export class DataLayerManagerService {
           const layers = this.mergeService.merge(this.state.layers, layer);
           this.state = { ...this.state, layers };
         },
-        error: (err) => this.loading$.next(false),
+        error: (err) => {
+          console.error(err);
+          this.loading$.next(false);
+        },
         complete: () => {
           this.loading$.next(false);
         },
