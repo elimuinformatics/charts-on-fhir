8cf53ee0b2fe5f45b71a3b0f6a4f948c113a2e4e
yashpmohod <ymohod@elimu.io>
2024-10-04 16:48:10 +0530

    fixed the showcase app unit test

--- apps/showcase/src/app/app.component.spec.ts
+++ apps/showcase/src/app/app.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
+import { DataLayerColorService, DataLayerManagerService, DataLayerService, DataLayerMergeService } from '@elimuinformatics/ngx-charts-on-fhir';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let colorService: DataLayerColorService;
+  let palette: string[] = ['#FFFFFF', '#121212', '#000000'];
 
   beforeEach(async () => {
+    colorService = new DataLayerColorService(palette);
     await TestBed.configureTestingModule({
-      imports: [AppModule],
+      imports: [AppComponent, NoopAnimationsModule],
+      providers: [
+        { provide: DataLayerMergeService, useClass: DataLayerMergeService },
+        { provide: DataLayerManagerService, useClass: DataLayerManagerService },
+        { provide: DataLayerService, useClass: DataLayerService },
+        { provide: DataLayerColorService, useValue: colorService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
