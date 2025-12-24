81726639627e97c4e563980dbb2358db2bef6e41
Yash Mohod <ymohod@elimu.io>
2025-12-24 13:23:45 +0530

    chore: update test script to use explicit Angular versions for compatibility

--- libs/ngx-charts-on-fhir/test-npm-package.sh
+++ libs/ngx-charts-on-fhir/test-npm-package.sh
@@ -4,28 +4,30 @@
 
 set -e
 
-echo ::::: Extracting minimum required angular version from package.json
-MIN_ANGULAR_VERSION=$(jq '.peerDependencies."@angular/core"' libs/ngx-charts-on-fhir/package.json | sed -E 's/">=(.*)"/\1/')
-echo Using Angular CLI $MIN_ANGULAR_VERSION
+# Set versions explicitly for testing
+MIN_ANGULAR_VERSION="20.3.15"
+MIN_ANGULAR_CLI_VERSION="20.3.13"
+MIN_MATERIAL_VERSION="20.2.14"
+echo Using Angular CLI $MIN_ANGULAR_CLI_VERSION
 
 echo :::: Packaging Charts-on-FHIR library
 cd dist/libs/ngx-charts-on-fhir
 PACKAGE_FILE=$(npm pack)
 cd ../../..
 
-echo ::::: Creating a new Angular app with Angular $MIN_ANGULAR_VERSION
-npx --yes \@angular/cli@${MIN_ANGULAR_VERSION} new test-app --defaults --package-manager=npm
+echo ::::: Creating a new Angular app with Angular $MIN_ANGULAR_CLI_VERSION
+npx --yes \@angular/cli@${MIN_ANGULAR_CLI_VERSION} new test-app --defaults --package-manager=npm
 cd test-app
 
 # Get the actual Angular version that was installed
 INSTALLED_ANGULAR_VERSION=$(jq -r '.dependencies."@angular/core"' package.json | sed 's/\^//')
 echo "Angular version installed: $INSTALLED_ANGULAR_VERSION"
 
-echo ::::: Installing Angular Material compatible with Angular $INSTALLED_ANGULAR_VERSION
-npm install @angular/material@20.2.14 @angular/cdk@20.2.14
+echo ::::: Installing Angular Material compatible with Angular $MIN_ANGULAR_VERSION
+npm install @angular/material@${MIN_MATERIAL_VERSION} @angular/cdk@${MIN_MATERIAL_VERSION}
 
-echo ::::: Pinning all Angular dependencies to version 20.x
-npm install @angular/core@20.3.15 @angular/common@20.3.15 @angular/platform-browser@20.3.15 @angular/platform-browser-dynamic@20.3.15 @angular/forms@20.3.15 @angular/router@20.3.15
+echo ::::: Pinning all Angular dependencies to version $MIN_ANGULAR_VERSION
+npm install @angular/core@${MIN_ANGULAR_VERSION} @angular/common@${MIN_ANGULAR_VERSION} @angular/platform-browser@${MIN_ANGULAR_VERSION} @angular/platform-browser-dynamic@${MIN_ANGULAR_VERSION} @angular/forms@${MIN_ANGULAR_VERSION} @angular/router@${MIN_ANGULAR_VERSION}
 
 echo ::::: Installing Charts-on-FHIR library
 npm i ../dist/libs/ngx-charts-on-fhir/${PACKAGE_FILE}
